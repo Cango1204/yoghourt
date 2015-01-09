38c6651ecfbd5259115b7b0237ecc54d903374d6
Cango1204 <hbjia1204@163.com>
2015-01-09 18:39:33 +0800

    my learning project

--- app/src/main/java/com/example/hbjia/ormlite/bean/User.java
+++ app/src/main/java/com/example/hbjia/ormlite/bean/User.java
@@ -1,7 +1,49 @@
 package com.example.hbjia.ormlite.bean;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by hbjia on 2015/1/9.
  */
+
+@DatabaseTable(tableName = "tb_user")
 public class User {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = "name")
+    private String name;
+    @DatabaseField(columnName = "desc")
+    private String desc;
+
+    public User(){}
+
+    public User(String name, String desc){
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
